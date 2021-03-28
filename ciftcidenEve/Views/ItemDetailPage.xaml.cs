27c776ba3157a979ea15ec8a21828daf11211c0a
gecicidegisken <hayallerimyaziyor@gmail.com>
2021-03-28 17:58:52 +0300

    Add project files.

---
+++ ciftcidenEve/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ciftcidenEve.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ciftcidenEve.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ ciftcidenEve/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ciftcidenEve.Models;
+using ciftcidenEve.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ciftcidenEve.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}

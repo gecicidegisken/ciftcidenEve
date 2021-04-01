01668ae7a1684bd545d3a9b89a8e4510a98310cb
gecicidegisken <hayallerimyaziyor@gmail.com>
2021-04-01 23:00:34 +0300

    Sign Up sayfası oluşturuldu ve HyperlinkSpan class'ı yazıldı

--- ciftcidenEve/ViewModels/SignUpViewModel.cs
+++ ciftcidenEve/ViewModels/SignUpViewModel.cs
@@ -1,16 +1,21 @@
-﻿using System.Windows.Input;
+﻿
+using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
+
 namespace ciftcidenEve.ViewModels
 {
-   public class SignUpViewModel : BaseViewModel
+  public  class SignUpViewModel : BaseViewModel
     {
+        public ICommand TapCommand;
         public SignUpViewModel()
         {
-            Title = "Üye ol";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            Title = "Uye ol";
+            TapCommand = new Command<string>(async (url) => await Launcher.OpenAsync(url));
+          
+
         }
-        public ICommand OpenWebCommand { get; }
+  
     }
 }

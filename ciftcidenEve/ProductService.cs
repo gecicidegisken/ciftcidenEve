1726cc9ed5c031091196d7124c63e4157816aa39
Mr Engineer <arifkaracalioglu01@gmail.com>
2021-04-01 23:03:39 +0300

    SQLite iskelet

---
+++ ciftcidenEve/ProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ciftcidenEve
+{
+    public static class ProductService
+    {
+
+        public static async Task addProduct(int id, string title, string description)
+        {
+
+        }
+    }
+   
+}

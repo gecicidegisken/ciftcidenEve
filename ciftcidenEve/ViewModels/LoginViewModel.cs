763521c247f7eaa9509c65a3c3947b53d5a5808e
gecicidegisken <hayallerimyaziyor@gmail.com>
2021-04-01 16:17:54 +0300

    Giriş yapma sayfası eklendi, Issues dosyası oluşturuldu.

--- ciftcidenEve/ViewModels/LoginViewModel.cs
+++ ciftcidenEve/ViewModels/LoginViewModel.cs
@@ -1,24 +1,30 @@
 ﻿using ciftcidenEve.Views;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xamarin.Forms;
+
 
 namespace ciftcidenEve.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
         public Command LoginCommand { get; }
+        public Command SignUpCommand { get; }
 
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
+            SignUpCommand = new Command(OnSignUpClicked);
         }
 
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+        } 
+        private async void OnSignUpClicked(object obj)
+        {
+            
+             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+             await Shell.Current.GoToAsync($"//{nameof(SignUpPage)}");
         }
     }
 }
---
+++ ciftcidenEve/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ciftcidenEve.ViewModels
+{
+   public class SignUpViewModel : BaseViewModel
+    {
+        public SignUpViewModel()
+        {
+            Title = "Üye ol";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+        public ICommand OpenWebCommand { get; }
+    }
+}

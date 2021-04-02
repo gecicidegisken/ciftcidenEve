836740bd9a8b81d53fad7de2aa97fb5fa8d8a4a0
Mr Engineer <arifkaracalioglu01@gmail.com>
2021-04-02 18:56:08 +0300

    added service

--- ciftcidenEve/Models/Item.cs
+++ ciftcidenEve/Models/Item.cs
@@ -4,7 +4,7 @@ namespace ciftcidenEve.Models
 {
     public class Item
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
     }
